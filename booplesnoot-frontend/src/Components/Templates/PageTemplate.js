fb32cda229cad236e1b5aa5e159ad0115f538e1e
Roxanne Barney <roxanne_barney@hotmail.com>
2019-11-30 13:11:35 +0800

    setting up account page

--- booplesnoot-frontend/src/Components/Templates/PageTemplate.js
+++ booplesnoot-frontend/src/Components/Templates/PageTemplate.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import Header from '../Molecules/Header';
-import Footer from '../Organisms/Footer';
-import NavBar from '../Molecules/NavBar'
+import React from "react";
+import Header from "../Molecules/Header";
+import Footer from "../Organisms/Footer";
+import NavBar from "../Molecules/NavBar";
 
-const PageTemplate = ({...props }) => {
-    return (
-        <div>
-            <Header/>
-            <NavBar/>
-            {props.children}
-            <Footer/>
-        </div>
-    )
-}
+const PageTemplate = ({ ...props }) => {
+  return (
+    <div>
+      <Header />
+      <NavBar />
+      {props.children}
+      <Footer />
+    </div>
+  );
+};
 
-export default PageTemplate; 
+export default PageTemplate;
--- booplesnoot-frontend/src/Containers/AppContainer.js
+++ booplesnoot-frontend/src/Containers/AppContainer.js
@@ -1,59 +1,48 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
-import HomePage from '../Pages/HomePage';
-import LoginPage from '../Pages/LoginPage';
-import RegisterPage from '../Pages/RegisterPage';
-import RecipePage from '../Pages/RecipePage';
-import FullRecipePage from '../Pages/FullRecipePage';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import HomePage from "../Pages/HomePage";
+import LoginPage from "../Pages/LoginPage";
+import RegisterPage from "../Pages/RegisterPage";
+import RecipePage from "../Pages/RecipePage";
+import FullRecipePage from "../Pages/FullRecipePage";
+import Account from "../Pages/Account";
 
 export const API_URL = "https://api.spoonacular.com";
 
-
-
-class AppContainer extends Component { 
-  render() {      
+class AppContainer extends Component {
+  render() {
     return (
       <>
         <Route
           path="/"
           component={() => {
-            return (
-              <HomePage/>
-            );
+            return <HomePage />;
           }}
           exact
         />
         <Route
           path="/Recipes"
           component={() => {
-            return (
-              <RecipePage/>
-            );
+            return <RecipePage />;
           }}
-        />    
+        />
         <Route
           path="/LoginPage"
           component={() => {
-            return (
-              <LoginPage/>
-            );
+            return <LoginPage />;
           }}
         />
         <Route
           path="/RegisterPage"
           component={() => {
-            return (
-              <RegisterPage/>
-            );
+            return <RegisterPage />;
           }}
         />
-        <Route
-          path="/FullRecipePage/:id"
-          component={FullRecipePage}
-        />
+        <Route path="/FullRecipePage/:id" component={FullRecipePage} />
+        <Route path="/Account" component={Account} />
       </>
     );
   }
 }
-    
+
 export default AppContainer;
